ba010e9d669cb1c635ce514b38be5a285b1f55fe
Hiago Fernandes <hiagofernands@gmail.com>
2023-02-13 08:41:20 -0300

    Add new project automation

---
+++ automate/automate/test_cases/PageElementsCorreios.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace automate.test_cases
+{
+    internal class PageElementsCorreios
+    {
+        public String HOME_PAGE = "https://www.correios.com.br/";
+        public By ziPCodeField = By.XPath("/html/body/div/div/div/div[3]/div/div/article/div[3]/div/div[2]/div[2]/form/div[2]/input");
+        public By btnCookie = By.Id("btnCookie");
+        public By btnSearch = By.XPath("//*[@id=\"content\"]/div[3]/div/div[2]/div[2]/form/div[2]/button/i");
+        public By messageCepFailedID = By.Id("mensagem-resultado-alerta");
+        public By messageCEPFailedXPATH = By.XPath("//*[@id=\"mensagem-resultado\"]");
+        public By messageCEPFailedCSS = By.CssSelector("#mensagem-resultado");
+        public By streetSearchCEP = By.XPath("//*[@id=\"resultado-DNEC\"]/tbody/tr/td[1]");
+        public By rastreamentoEncomenda = By.XPath("//*[@id=\"objetos\"]");
+    }
+}
---
+++ automate/automate/test_cases/TestBuscaCEP.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace automate.test_cases
+{
+    [TestFixture]
+    public class TestBuscaCEP
+    {
+        IWebDriver? driver;
+        PageElementsCorreios pg = new PageElementsCorreios();
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void TestCEPCorreios()
+        {
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(pg.HOME_PAGE);
+
+            waitByElement(pg.btnCookie);
+
+            driver.FindElement(pg.btnCookie).Click();
+
+            waitByElement(pg.ziPCodeField);
+
+            driver.FindElement(pg.ziPCodeField).SendKeys("80700000" + Keys.Return);
+
+            driver.SwitchTo().Window(driver.WindowHandles[1]);
+
+            waitByElement(pg.messageCepFailedID); 
+
+            Assert.IsTrue(driver.FindElement(pg.messageCepFailedID).Displayed);
+            Assert.IsTrue(driver.FindElement(pg.messageCEPFailedCSS).Displayed);
+
+            driver.Close();
+            driver.SwitchTo().Window(driver.WindowHandles[0]);
+
+            driver.Navigate().GoToUrl(pg.HOME_PAGE);
+
+            waitByElement(pg.ziPCodeField);
+            driver.FindElement(pg.ziPCodeField).SendKeys("01013-001" + Keys.Return);
+
+            driver.SwitchTo().Window(driver.WindowHandles[1]);
+
+            waitByElement(pg.streetSearchCEP);
+
+            String expectedStreet = "Rua Quinze de Novembro - lado ímpar";
+            String actualStreet = driver.FindElement(pg.streetSearchCEP).Text;
+
+            Assert.AreEqual(expectedStreet, actualStreet);
+        }
+
+        [TearDown]
+        public void close_Browser()
+        {
+            driver.Quit();
+        }
+
+        public void waitByElement(By element)
+        {
+            int timeOut = 60;
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeOut));
+            IWebElement myElement = wait.Until(x => x.FindElement(element));
+        }
+    }
+}
